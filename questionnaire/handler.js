c4eabe1d11e7b3bdfa9dbeca0ec6eb277c4de2a6
Nimord Shory <nimrod@julbox.com>
2018-02-20 15:51:00 +0200

    json response

--- questionnaire/handler.js
+++ questionnaire/handler.js
@@ -1,7 +1,6 @@
 'use strict';
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const fse = require('fs-extra');
 const questionBucket = "thanos-fuse";
 
 module.exports.getQuestionnaire = (event, context, callback) => {
@@ -9,11 +8,20 @@ module.exports.getQuestionnaire = (event, context, callback) => {
     const key = event.queryStringParameters.id + ".json";
     let json;
 
+    const response = {
+        statusCode: 200,
+    };
+
+    console.log("Getting s3 object");
+
     s3.getObject({Bucket: questionBucket, Key: key})
         .promise()
         .then((data) => {
+            console.log("Got s3 object", data);
             json = JSON.parse(new Buffer(data.Body).toString("utf8"));
-            callback(null, json);
+            console.log("JSON", json);
+            response.body = JSON.stringify(json);
+            return callback(null, response);
         })
         //.then(() => {
         //})
