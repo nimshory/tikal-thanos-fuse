88bbc3094d147a61b1592a693dbecfd7d5207497
Nimord Shory <nimrod@julbox.com>
2018-02-20 15:09:40 +0200

    json parsing

--- questionnaire/handler.js
+++ questionnaire/handler.js
@@ -1,16 +1,24 @@
 'use strict';
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fse = require('fs-extra');
+const questionBucket = "thanos-fuse";
 
 module.exports.getQuestionnaire = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Thanos Team!',
-      input: event
-    })
-  };
 
-  callback(null, response);
+    const key = event.queryStringParameters.id + ".json";
+    let json;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    s3.getObject({Bucket: questionBucket, Key: key})
+        .promise()
+        .then((data) => {
+            json = JSON.parse(new Buffer(data.Body).toString("utf8"));
+            callback(null, json);
+        })
+        //.then(() => {
+        //})
+        .catch(err => {
+            return callback(err);
+        });
+
 };
